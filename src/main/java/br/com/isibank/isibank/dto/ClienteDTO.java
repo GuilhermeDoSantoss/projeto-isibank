bc60425744f9b00f44e0e27f12532838162490ed
Guilherme dos Santos <guilherme1996santos@icloud.com>
2025-09-18 21:43:46 -0300

    Criando model cliente e clienteDTO

--- src/main/java/br/com/isibank/isibank/dto/ClienteDTO.java
+++ src/main/java/br/com/isibank/isibank/dto/ClienteDTO.java
@@ -1,4 +1,28 @@
 package br.com.isibank.isibank.dto;
 
-public class ClienteDTO {
+import br.com.isibank.isibank.model.Cliente;
+
+public record ClienteDTO(String nome, String email, String cpf, String telefone, String senha) {
+
+    public Cliente toCliente(){
+        Cliente cliente = new Cliente();
+        cliente.setCpf(cpf);
+        cliente.setNome(nome);
+        cliente.setEmail(email);
+        cliente.setTelefone(telefone);
+        cliente.setTelefone(telefone);
+        return cliente;
+    }
 }
+
+
+// DTO (Data Transfer Object) → Classe tradicional usada para transportar dados entre camadas.
+// Normalmente tem getters/setters, pode ter validações, conversões, anotações do Jackson/Bean Validation etc.
+// Mais flexível, mas gera muito boilerplate.
+
+// Record → Tipo imutável criado para carregar dados de forma enxuta.
+// O compilador já gera equals, hashCode, toString, getters e construtor.
+// Ideal para DTOs simples e imutáveis. Porém, é mais restrito (não tem setters, herança limitada).
+
+// DTO = Verbosidade, mas customizável.
+// Record = Enxuto, imutável, menos burocracia.
--- src/main/java/br/com/isibank/isibank/model/Cliente.java
+++ src/main/java/br/com/isibank/isibank/model/Cliente.java
@@ -1,4 +1,77 @@
 package br.com.isibank.isibank.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tbl_cliente")
 public class Cliente {
+
+//nullable = false → constraint de NOT NULL. O campo é obrigatório, não aceita valores nulos no banco.
+//unique = true → cria um índice único. Garante que nenhum registro terá o mesmo valor nesse campo (exemplo: não podem existir dois clientes com o mesmo e-mail).
+//length = 100 → define o tamanho máximo da coluna no banco de dados, ou seja, VARCHAR(100).
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_cliente")
+    private Integer idCliente;
+    @Column(name = "nome_cliente", nullable = false, length = 150)
+    private String nome;
+    @Column(name = "email_cliente", nullable = false, unique = true, length = 100)
+    private String email;
+    @Column(name = "cpf_cliente", nullable = false, unique = true, length = 11)
+    private String cpf;
+    @Column(name = "telefone_cliente", nullable = false, unique = true, length = 20)
+    private String telefone;
+    @Column(name = "senha_cliente", nullable = false, length = 200)
+    private String senha;
+
+
+    public Integer getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(Integer idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
 }
+
